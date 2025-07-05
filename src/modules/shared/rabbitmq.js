93675aff675bf5a383b6a894dcb4024aad8c09d9
Priyanka Das <priyankadas@Priyankas-MacBook-Air.local>
2025-07-06 01:39:06 +0530

    fix: rabbitmq connection

--- src/modules/shared/rabbitmq.js
+++ src/modules/shared/rabbitmq.js
@@ -3,24 +3,34 @@ const amqp = require('amqplib');
 let channel = null;
 
 async function connectRabbitMQ() {
-  const connection = await amqp.connect(process.env.RABBITMQ_URL);
-  channel = await connection.createChannel();
-  await channel.assertQueue('order_queue');
-  console.log('✅ Connected to RabbitMQ');
+  try {
+    const connection = await amqp.connect(process.env.RABBITMQ_URL);
+    channel = await connection.createChannel();
+    await channel.assertQueue("order_queue");
+    console.log("✅ Connected to RabbitMQ");
+  } catch (error) {
+    console.error("❌ RabbitMQ connection failed:", error.message);
+    throw error;
+  }
+}
+
+function getChannel() {
+  if (!channel) throw new Error('RabbitMQ channel not established');
+  return channel;
 }
 
 function publishToQueue(queueName, msg) {
-  if (!channel) throw new Error('RabbitMQ channel not established');
+  const channel = getChannel();
   channel.sendToQueue(queueName, Buffer.from(JSON.stringify(msg)));
 }
 
 function consumeQueue(queueName, onMessage) {
-  if (!channel) throw new Error('RabbitMQ channel not established');
-  channel.consume(queueName, async (msg) => {
-    const data = JSON.parse(msg.content.toString());
-    await onMessage(data);
-    channel.ack(msg);
-  });
+    const channel = getChannel();
+    channel.consume(queueName, async (msg) => {
+      const data = JSON.parse(msg.content.toString());
+      await onMessage(data);
+      channel.ack(msg);
+    });
 }
 
 module.exports = {
